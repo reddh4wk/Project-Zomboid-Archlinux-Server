67895b89a8f441c69a4a6e50c81a064b0c4bde03
Damiano Meda <damiano.meda@gmail.com>
2024-02-12 12:37:45 +0100

    Create pzserver.sh

---
+++ opt/pzserver/pzserver.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOG_FILE="/opt/pzserver/pzserver.log"
+SESSION_NAME="pzserver"
+SERVER_START_CMD="/opt/pzserver/start-server.sh --servername '[PvPvE] Menzian Fields' | tee $LOG_FILE"
+TIMEOUT=60
+
+case "$1" in
+  --see)
+    tmux attach-session -t $SESSION_NAME
+    ;;
+  --start)
+    /opt/pzserver/python/bin/python /opt/pzserver/telegram_bot/pzserver_main.py >> /opt/pzserver/telegram_bot/pzserver_main.log &
+    /opt/pzserver/python/bin/python /opt/pzserver/telegram_bot/pzserver_log_parser.py >> /opt/pzserver/telegram_bot/pzserver_log_parser.log &    
+    tmux new-session -d -s $SESSION_NAME "$SERVER_START_CMD"
+    ;;
+  --stop)
+    tmux send-keys -t $SESSION_NAME "save" C-m
+    tmux send-keys -t $SESSION_NAME "quit" C-m
+
+    # Attendi fino a quando il processo pzserver termina o timeout
+    timeout=0
+    while [ $timeout -lt $TIMEOUT ]; do
+      if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+       	sleep 1 && ((timeout++))
+      else
+        break
+      fi
+    done
+    if [ $timeout -eq $TIMEOUT ]; then
+      echo "Server timeout exception"
+    else
+      echo "Server has been terminated."
+    fi
+    ;;
+  *)
+    echo "Usage: pzserver --see | --start | --stop"
+    exit 1
+    ;;
+esac
