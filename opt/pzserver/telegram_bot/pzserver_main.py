57c1a6d2c8b5ec1fd00a00683ed419832db825b7
Damiano Meda <damiano.meda@gmail.com>
2024-02-12 12:39:08 +0100

    Create pzserver_main.py

---
+++ opt/pzserver/telegram_bot/pzserver_main.py
@@ -0,0 +1,269 @@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    (@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.    @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@, %@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ,@@@@@@@@@@@@@@@@@@@@@  @@@@@        @@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@%        @@       @@      @@   /@@&     .@@@     #%    /@@@@  .      @@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@%  /@@  .@&  #@@@@  ,@@,  @&  /@@  .@@  /@.  @@@@@@  *@@@@@, @@      @@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@(  /@@  ,@@  #@@@%  *@@   @@  (@&  ,@@@@@@  .@@@@@@  *@@@@@  @.    ,&*@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@(  ,@@  #@@  #@@@@, .@@  #@@  (@@*  @@@(/@(  @@@ /@  *@@@@@@%   (. .@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@.  /@@@@@@@@@@@@@@@@@@@@@#@%  %@@@@@@@@@@@@@@@@@@@@@@@@@@@@,  .@@%  @@@@@@@@@@@@@@@@@@@@@@@@@
+#@                 @@@@@@@@.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@. @@&&&@@@@@@@@@@@@@@@@@@   (@@@  (@@@@@@@@@@@@@@@@@@@@@@@@@
+#                  /@@%     .%@@@@@@@@@@@@@@@@@@@@@@@@@@@*            .@@@@@@@@@@       (@@@(  #@@@@           ,/@@@@@@@@
+#      &@@@@@     *@@     .    &@       .@@@@@@@@        @#    /@@@.    &@@@@@          .@@@@.      @%     .*.     %@@@@@
+#     &@@@@@      @@,   &@@&    @@       /@@@@@@       ,@@%    %@@@@     #@@,     @@@/    #@@@    .@@@     @@@@*    @@@@@
+#    &@@@@@      (@@   .@@@@    /@(      ,@@@@@/       (@@*    (@@@@(    ,@@.    @@@@@     %@@    &@@@     @@@@@     &@@@
+#   *@@@@@#      @@.   %@@@@    .@(       &@@@@        #@@@    /@@@@.    %@@     @@@@@*    (@@    @@@#     @@@@@#    .@@@
+#/ @@@@@@@      @@@    &@@@@     @#       .@@@@        /@@#    (@@@.     @@*     @@@@@     ,@@    %@@@     @@@@@@     @@@
+#@@@@@@@@      &@@@    %@@@@     @#        #@@#        ,@@#    ,@@(     @@@     /@@@@@.     @@    #@@@     &@@@@&     @@@
+#@@@@@@@      #@@@,    #@@@@     &#        *@@*        .@@@           (@@@@     %@@@@@*     %@    *@@@     (@@@@      %@@
+#@@@@@@&     *@@@@,    (@@@@/    @#   .     @%          @@#    (@@@     %@@     &@@@@@@     ,@.   /@@&     /@@@@      /@@
+#@@@@@@      %@@@@,    /@@@@    ,@#   %@.        @#    .@@*    %@@@@,    #@     #@@@@@@     %@.   #@@@     (@@@@      ,@@
+#@@@@@      *@@@@@@,   /@@@@    (@(   @@/       (@,    ,@@*    #@@@@/    ,@,    /@@@@@      @@    %@@@     @@@@@@     (@@
+#@@@@      .@@@@@@@@   .@@@%    /@(   @@@       @@     *@@*    %@@@@     ,@*    ,@@@@@     ,@@    %@@@     @@@@@#     @@@
+#@@@@      @@@@@@@( (    #/   .@@@,   (@@.     .@@.    (@@,    &@@@@     (@(    .@@@@@     #@@.   /@@@     @@@@@     #@@@
+#@@@      %@@@@@@   /@@.    (@@@@@    .@@@     ,@@(    &@@,    %@@@      @@@    .@@@@@     &@&    .@@&.    @@@@@     @@@@
+#@@      /@@@@@@    @@@@@@@@@@@@@@/   .@@@,    %@@     /@             (@@@@@@.   (@@@.     @@.     /@@    ,@@@@     @@@@@
+#@      ,@@@@@@     @@@@@@@@@@@@@@    /@@@&   ,@@@     ,@@@@@@@@@@&@@@@@@@@@@@    ,@/    /@@@@@@@@@@@.      /     *@@@@@@
+#@      @@@@@@      @@@@@@@@@@@@@      &@@@   @@@(      %@###%.,/###%@((    #@@@,      *@@@@@@@@@@@@@/ ,((,..*/%@@@@@@@@@
+#                   &@@@@@@@@@@@@(,(#(*@@@@@@@@@@( ,//*,&@@                     /@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+##(*,,. ..,,*****,*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&##                         .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         .%*/@@@( (%#/          /#&( &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@     .#(   / .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# Please install pip on your system. Use your package maanger if you're using Linux or this if you're using Windows:
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
+#
+#
+# python ./main.py (assuming you are positioned in the same folder of the .py file)
+#
+# Oh, also remember to get a telegram bot TOKEN from the botfather bot and replace the MAIN_TOKEN variable value with it.
+# Also make sure your bot is in your public group with admin priviledges and disable the privacy setting of your bot from botfather chat using /setprivacy command or whatever it is.
+# I think this should be it...
+
+########################################
+### AVOID DOUBLE EXECUTION OF THIS SCRIPT ON THE CURRENT SYSTEM
+########################################
+
+def get_pid_and_name():
+    import os
+    import sys
+    return (os.getpid(), os.path.abspath(sys.argv[0]))
+
+def run_command(command):
+    import subprocess
+    try:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        output, error = process.communicate()
+        if process.returncode != 0:
+            print(f"Error executing command: {error}")
+            return None
+        return output
+    except Exception as e:
+        print(f"Error executing command: {e}")
+        return None
+
+def is_already_running(current_pid, current_script):
+    processes = run_command('ps aux | grep ' + current_script)
+    if processes:
+        lines = processes.split('\n')
+        for line in lines:
+            if current_script in line and 'python' in line:
+                pid = int(line.split()[1])
+                if pid != current_pid:
+                    return True
+    return False
+
+########################################
+### INFORMATION FOR THE LOG FILE
+########################################
+
+def log_file():
+    import os
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), os.path.splitext(os.path.basename(__file__))[0]+'.log')
+
+def clean_logs(max_rows=1000, buffer=300, log_file=log_file()):
+    with open(log_file, 'r') as f:
+        lines = f.readlines()
+        if len(lines) >= max_rows:
+            lines = lines[-(max_rows - buffer):]
+        with open(log_file, 'w') as f:
+            f.writelines(lines)
+
+def logger(message, msg_type, log_file=log_file()):
+    import os
+    import traceback
+    import datetime
+    with open(log_file, 'a') as f:
+        timestamp = datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S] ')
+        f.write(timestamp+"["+msg_type+"] "+str(message)+'\n')
+        traceback_str = traceback.format_exc()
+        if traceback_str:
+            f.write(timestamp+"[TRACEBACK] "+traceback_str)
+
+########################################
+### INFORMATION FOR LOG PARSER
+########################################
+
+def get_chats():
+    return [-1002033569385] # if message.chat.id in chat_ids
+
+def get_token():
+    return '6353909839:AAEQRzoSX9pXCL0sCtRUiw0aWW4via9bgzU'
+
+def get_servicename():
+    return 'pzserver'
+
+chat_ids=get_chats()
+TOKEN=get_token()
+
+########################################
+### STATUS CHECKER (FOR THE MONITOR see systemctl_status_change_notify.py)
+########################################
+
+
+def check_service_status(service_name=get_servicename()):
+    import subprocess
+    try:
+        # Run systemctl status <service_name> command
+        result = subprocess.run(['systemctl', 'status', service_name], capture_output=True, text=True)
+        output_lines = result.stdout.split('\n')
+        if 'Active: active (running)' in output_lines[2]:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logger(e, "ERROR")
+        return None
+
+########################################
+### MAIN
+########################################
+
+global_commands = []
+
+if __name__ == '__main__':
+    import asyncio
+    import telebot
+    from telebot.async_telebot import AsyncTeleBot
+    bot = AsyncTeleBot(TOKEN)
+
+    ########################################
+    ### TELEBOT - CHECKS
+    ########################################
+
+    async def member_is_admin(message):
+        member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+        if member.status in ['creator','administrator']:
+            return True
+        else:
+            await bot.reply_to(message, "This command requires administrator-level or superior priviledges.")
+            return False
+
+    async def save_user_command(cmd, user):
+        global global_commands
+        for each in global_commands:
+            if each[1] == user:
+                each[0] = cmd
+                return
+        global_commands.append((cmd, user))        
+
+    async def check_user_command(cmd, user):
+        global global_commands
+        for each in global_commands:
+            if each[0] == cmd and each[1] == user:
+                return True
+        return False
+
+    async def clear_user_command(user):
+        global global_commands
+        temp = global_commands
+        for each in global_commands:
+            if each[1] == user:
+                temp.remove(each)
+                removed = True
+        global_commands = temp
+        print(global_commands)
+        if removed:
+            return True
+        else:
+            return False
+
+    ########################################
+    ### TELEBOT - HANDLERS
+    ########################################
+
+    # START
+    @bot.message_handler(commands=['start'])
+    async def send_start(message):
+        await bot.reply_to(message, """\
+    Hi, I am fucknurdy! I can automatically get you updates on the status of the PZserver.
+    """)
+
+    # HELP
+    @bot.message_handler(commands=['help'])
+    async def send_help(message):
+        await bot.reply_to(message, """
+    List of the commands:
+    /help: Display this message.
+    /status: Display the current status of the PZserver.
+    /restart: Reboots the server
+    """)
+
+    # STATUS
+    @bot.message_handler(commands=['status'])
+    async def send_status(message):
+        service_status = check_service_status()
+        if service_status:
+            status = "active."
+        elif not service_status:
+            status = "down."
+        else:
+            status = "???"
+        await bot.reply_to(message, "The PZserver is currently "+status)
+
+    # RESTART
+    @bot.message_handler(commands=['restart'])
+    async def restart_server(message):
+        is_admin = await member_is_admin(message)
+        if is_admin:
+            if check_service_status():
+                await save_user_command('restart', message.from_user.id)
+                print(global_commands)
+                await bot.reply_to(message,  """
+    Are you really sure you want to restart the server? All users will be disconnected.
+    
+Please press: /confirm_restart to proceed.
+
+Or press: /cancel_restart to cancel.
+""")
+            else:
+                await bot.reply_to(message, "The server is currently down.")
+
+    @bot.message_handler(commands=['confirm_restart'])
+    async def confirm_restart(message):
+        restart_required = await check_user_command('restart', message.from_user.id)
+        if restart_required:
+            await clear_user_command(message.from_user.id)
+            print(global_commands)
+            await run_command("sudo systemctl restart " + get_servicename())
+
+    @bot.message_handler(commands=['cancel_restart'])
+    async def confirm_restart(message):
+        await clear_user_command(message.from_user.id)
+        print(global_commands)
+
+    ########################################
+    ### MAIN - START
+    ########################################
+
+    try:
+        pid, name = get_pid_and_name()
+        if is_already_running(pid, name):
+            print("Another instance of the script is already running.")
+            sys.exit(1)
+        clean_logs()
+        asyncio.run(bot.infinity_polling())
+    except Exception as e:
+        logger(e, "ERROR")
