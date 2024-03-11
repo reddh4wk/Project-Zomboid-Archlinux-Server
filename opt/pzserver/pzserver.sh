7be3f077c60bdf0b477afe61331eb84e287d0757
Damiano Meda <damiano.meda@gmail.com>
2024-03-11 20:19:41 +0100

    Update pzserver.sh

--- opt/pzserver/pzserver.sh
+++ opt/pzserver/pzserver.sh
@@ -1,26 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-LOG_FILE="/opt/pzserver/pzserver.log"
-SESSION_NAME="pzserver"
-SERVER_START_CMD="/opt/pzserver/start-server.sh --servername '[PvPvE] Menzian Fields' | tee $LOG_FILE"
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+LOG_FILE="$DIR/pzserver.log"
+SERVICE_NAME=$(basename "$DIR")
+SERVER_START_CMD="$DIR/start-server.sh -servername $SERVICE_NAME | tee $LOG_FILE"
 TIMEOUT=60
+PYTHON_VENV_BIN="$DIR/python/bin/python"
 
 case "$1" in
   --see)
-    tmux attach-session -t $SESSION_NAME
+    tmux attach-session -t $SERVICE_NAME
     ;;
   --start)
-    /opt/pzserver/python/bin/python /opt/pzserver/telegram_bot/pzserver_tg.py >> /opt/pzserver/telegram_bot/pzserver_tg.log &    
-    tmux new-session -d -s $SESSION_NAME "$SERVER_START_CMD"
+    $PYTHON_VENV_BIN $DIR/telegram_bot/pzserver_tg.py >> $DIR/telegram_bot/pzserver_tg.log &
+    tmux new-session -d -s $SERVICE_NAME "$SERVER_START_CMD"
     ;;
   --stop)
-    tmux send-keys -t $SESSION_NAME "save" C-m
-    tmux send-keys -t $SESSION_NAME "quit" C-m
+    tmux send-keys -t $SERVICE_NAME "save" C-m
+    tmux send-keys -t $SERVICE_NAME "quit" C-m
 
     # Attendi fino a quando il processo pzserver termina o timeout
     timeout=0
     while [ $timeout -lt $TIMEOUT ]; do
-      if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+      if tmux has-session -t $SERVICE_NAME 2>/dev/null; then
        	sleep 1 && ((timeout++))
       else
         break
