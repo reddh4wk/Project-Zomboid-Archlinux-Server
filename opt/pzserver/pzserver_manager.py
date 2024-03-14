cb4f2cb2ca0b87588e32f13f22a017d7069d7275
Damiano Meda <damiano.meda@gmail.com>
2024-03-14 03:25:01 +0100

    Create pzserver_manager.py

---
+++ opt/pzserver/pzserver_manager.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+LOG_FILE="$DIR/pzserver.log"
+SERVICE_NAME=$(basename "$DIR")
+SERVER_START_CMD="$DIR/start-server.sh -servername $SERVICE_NAME | tee $LOG_FILE"
+TIMEOUT=60
+
+case "$1" in
+  --see)
+    tmux attach-session -t $SERVICE_NAME
+    ;;
+  --start)
+    tmux new-session -d -s $SERVICE_NAME "$SERVER_START_CMD"
+    ;;
+  --stop)
+    tmux send-keys -t $SERVICE_NAME "save" C-m
+    tmux send-keys -t $SERVICE_NAME "quit" C-m
+
+    # Attendi fino a quando il processo pzserver termina o timeout
+    timeout=0
+    while [ $timeout -lt $TIMEOUT ]; do
+      if tmux has-session -t $SERVICE_NAME 2>/dev/null; then
+       	sleep 1 && ((timeout++))
+      else
+        break
+      fi
+    done
+    if [ $timeout -eq $TIMEOUT ]; then
+      echo "Server timeout exception"
+    else
+      echo "Server has been terminated."
+    fi
+    ;;
+  *)
+    echo "Usage: pzserver --see | --start | --stop"
+    exit 1
+    ;;
+esac
